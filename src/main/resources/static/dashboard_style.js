3fc03b224c761f0805a96ba74b56af420af3d5f3
JENGMINJAE <159225415+JENGMINJAE@users.noreply.github.com>
2024-10-08 11:53:25 +0900

    Update dashboard_style.js

--- src/main/resources/static/dashboard_style.js
+++ src/main/resources/static/dashboard_style.js
@@ -36,7 +36,6 @@ for (var i = 0; i < markerPositions.length; i++) {
     map: map, // 마커를 표시할 지도
     position: markerPositions[i].latlng, // 마커를 표시할 위치
     title: markerPositions[i].title // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-
   });
 }
 // 마커가 지도 위에 표시되도록 설정합니다
